87de529bb3e00e4554668e731123d4bea9adab6c
gopika <gopikamanoj001@gmail.com>
2024-07-02 19:37:04 +0530

    modules cleraed

--- Backend/Src/Config/config.js
+++ Backend/Src/Config/config.js
@@ -2,6 +2,6 @@
 export default {
     port: process.env.PORT || 3000,
     mongo: {
-      uri: 'mongodb://127.0.0.1:27017/password-generator',
+      uri: 'mongodb://GopikaP:uzoweAZaEpuoEfMe@ac-tnu743v-shard-00-00.rvcgyot.mongodb.net:27017,ac-tnu743v-shard-00-01.rvcgyot.mongodb.net:27017,ac-tnu743v-shard-00-02.rvcgyot.mongodb.net:27017/?ssl=true&replicaSet=atlas-d9s3w4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0',
     },
 };
