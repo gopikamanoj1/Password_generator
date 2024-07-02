7d8cccc746cc87c49886b1e37665345743e2bc0f
gopika <gopikamanoj001@gmail.com>
2024-07-02 20:32:37 +0530

    syntax corrected

--- Backend/Src/Frameworks/Repository/index.js
+++ Backend/Src/Frameworks/Repository/index.js
@@ -1,6 +1,6 @@
 // Repository/index.js
 
-import {userRepository} from "../Repository/userRepository.js";
+import userRepository from "../Repository/userRepository.js";
 export {
     userRepository
 }  
